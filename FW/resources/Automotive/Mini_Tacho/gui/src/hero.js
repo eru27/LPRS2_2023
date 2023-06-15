12b96e2f161fdb1a7775652693315310aaf3a737
Julija Jelicanin <julijajelicanin@Julijas-MacBook-Air.local>
2023-06-15 12:05:06 +0200

    Gui update

--- FW/resources/Automotive/Mini_Tacho/gui/src/hero.js
+++ FW/resources/Automotive/Mini_Tacho/gui/src/hero.js
@@ -2,6 +2,7 @@ import './hero.css'
 import heroImage from'../src/resources/delivery.png';
 import MyJson from './jsonComponent';
 import GraphComponent from './graphComponent';
+import './jsonComponent.css'
 
 function Hero() {
     const MovingDiv = () => {
@@ -14,7 +15,23 @@ function Hero() {
             <section className='section heroSection'>
                 <div className='container heroContainer'>
                 <MovingDiv/>
-                <MyJson/>
+                {/* <MyJson/> */}
+                <div className='data'>
+                <div className='col leftColumn'>
+                    <p className='jsonItem' >Duzina trajanja</p>
+                    <p className='jsonItem' >Maksimalna brzina</p>
+                    <p className='jsonItem' >Predjena daljina</p>
+                    <p className='jsonItem' >Prosecna brzina</p>
+                
+                </div>
+                <div className='col rightColumn'>
+                    <p className='jsonItem' >Duzina trajanja</p>
+                    <p className='jsonItem' >Maksimalna brzina</p>
+                    <p className='jsonItem' >Predjena daljina</p>
+                    <p className='jsonItem' >Prosecna brzina</p>
+                
+                </div>
+                </div>
                 <GraphComponent/>
                 </div>
             </section>
--- FW/resources/Automotive/Mini_Tacho/gui/src/jsonComponent.js
+++ FW/resources/Automotive/Mini_Tacho/gui/src/jsonComponent.js
@@ -2,12 +2,17 @@ import React from 'react';
 import './jsonComponent.css'
 import jsonData from './data.json';
 
+
 const MyJson = () => {
+
   return (
       <div className='data'>
         {jsonData.items.map(item => (
           <p className='jsonItem' key={item.id}>{item.name}</p>
+          
         ))}
+
+        
       </div>
   );
 };
