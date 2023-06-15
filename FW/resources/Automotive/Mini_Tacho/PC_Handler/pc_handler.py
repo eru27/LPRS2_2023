b4ad79d55b8bd1c9e3925f442362a789f593c202
Anja Ekres <anja.ekres@gmail.com>
2023-06-15 11:13:01 +0200

    added output to handler

--- FW/resources/Automotive/Mini_Tacho/PC_Handler/pc_handler.py
+++ FW/resources/Automotive/Mini_Tacho/PC_Handler/pc_handler.py
@@ -1,5 +1,6 @@
 import json
 import re
+from matplotlib import pyplot as plt
 
 rpms = []
 line = input()
@@ -7,8 +8,24 @@ while line != 'FATAL: stdin closed':
     data = re.search('(?P<count>[0-9]+) cnt[\t](?P<rpm>[0-9]+) rpm', line)
     if data:
         #print(data.groupdict())
-        rpms.append(data.groupdict()['rpm'])
+        rpms.append(int(data.groupdict()['rpm']))
     line = input()
 
-x = list(range(len(rpms)))
+rpmsFixed = []
 
+mask = 100
+
+for i in range(mask, len(rpms) - mask):
+    sum = 0
+    for data in rpms[i - mask:i + mask]:
+        sum += data
+    rpmsFixed.append(sum / ((mask * 2) + 1))
+
+speed = []
+
+for rpm in rpmsFixed:
+    speed.append((130 * rpm) / 5000)
+
+#plt.plot(rpmsFixed)
+plt.plot(speed)
+plt.savefig('speed.png')
